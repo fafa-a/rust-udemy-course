75e89e041dab1140f0af65202f19d3e231cf959c
Puech Fabien (CSgroup)~ <fabien.puech@csgroup.eu>
2023-01-23 11:56:09 +0100

    mulitple traits fn

--- base_rust/src/mon_module/traits.rs
+++ base_rust/src/mon_module/traits.rs
@@ -1,40 +1,62 @@
-trait Compute {
+trait Area {
     fn area(&self) -> f32;
+}
+
+trait Perimeter {
+    fn perimeter(&self) -> f32;
 }
 
 struct Rectangle {
     width: f32,
-    height: f32,
+    length: f32,
 }
 
-impl Compute for Rectangle {
+impl Area for Rectangle {
     fn area(&self) -> f32 {
-        self.width * self.height
+        self.width * self.length
     }
 }
 
-struct Circle {
-    radius: f32,
-}
-
-impl Compute for Circle {
-    fn area(&self) -> f32 {
-        &self.radius.powf(2.0) * std::f32::consts::PI
+impl Perimeter for Rectangle {
+    fn perimeter(&self) -> f32 {
+        2.0 * (self.width + self.length)
     }
 }
+
+impl Rectangle {
+    fn hello(&self) {
+        println!("Hello");
+    }
+}
+
+// struct Circle {
+//     radius: f32,
+// }
+//
+// impl Compute for Circle {
+//     fn area(&self) -> f32 {
+//         &self.radius.powf(2.0) * std::f32::consts::PI
+//     }
+// }
 
 pub fn hello() {
     let r = Rectangle {
         width: 10.0,
-        height: 20.0,
+        length: 20.0,
     };
-    let c = Circle { radius: 10.0 };
-    computed(r)
-    computed(c);
+    let r2 = Rectangle {
+        width: 20.0,
+        length: 30.0,
+    };
+
+    computed(r);
 }
-// fn computed<T: Compute>(figure: T) {
+
+// fn computed<T: Area + Perimeter>(figure: T) {
 //    println!("{}", figure.area());
 //    }
-fn computed(figure: impl Compute) {
+
+fn computed(figure: impl Area + Perimeter) {
     println!("{}", figure.area());
+    print!("{}", figure.perimeter());
 }
