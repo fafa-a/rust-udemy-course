a329d9cd59a5fcefca192a4e8e7b2b81481fa26e
Puech Fabien (CSgroup)~ <fabien.puech@csgroup.eu>
2023-01-23 16:06:10 +0100

    struct and lifetime

--- base_rust/src/mon_module/lifetime.rs
+++ base_rust/src/mon_module/lifetime.rs
@@ -1,7 +1,12 @@
+struct ToSay<'a> {
+    text: &'a str,
+}
 pub fn hello() {
-    let a = 8;
-    let b = 32;
-    println!("{}", greather_than(&a, &b));
+    // let a = 8;
+    // let b = 32;
+    // println!("{}", greather_than(&a, &b));
+    let a = ToSay { text: "Hello" };
+    println!("{}", a.text);
 }
 
 fn greather_than<'a>(a: &'a u8, b: &'a u8) -> &'a u8 {
