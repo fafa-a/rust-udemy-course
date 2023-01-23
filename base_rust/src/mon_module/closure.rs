c922fd5aa774fcca736db59a5088ce0a34433ae5
Puech Fabien (CSgroup)~ <fabien.puech@csgroup.eu>
2023-01-23 16:32:16 +0100

    closure fn

--- base_rust/src/mon_module/closure.rs
+++ base_rust/src/mon_module/closure.rs
@@ -1,17 +1,16 @@
 pub fn hello() {
     let a = 8;
     let b = 32;
-    compute(add, a, b);
-    compute(mulitply, a, b);
+    compute(|x, y| x + y, a, b);
 }
 
-fn add(x: i32, y: i32) -> i32 {
-    x + y
-}
-
-fn mulitply(x: i32, y: i32) -> i32 {
-    x * y
-}
+// fn add(x: i32, y: i32) -> i32 {
+//     x + y
+// }
+//
+// fn mulitply(x: i32, y: i32) -> i32 {
+//     x * y
+// }
 
 fn compute<F>(function: F, a: i32, b: i32)
 where
