e1229de9c1bdb5b7d6d44146198e420e8721eeda
fafa-a <fabienpuech1@gmail.com>
2023-01-23 22:09:11 +0100

    closure borow

--- base_rust/src/mon_module/closure.rs
+++ base_rust/src/mon_module/closure.rs
@@ -1,23 +1,10 @@
 pub fn hello() {
-    let a = 8;
-    let b = 32;
-    // compute(|x, y| x + y, a, b);
-    compute(|| a + b);
-}
-
-// fn add(x: i32, y: i32) -> i32 {
-//     x + y
-// }
-//
-// fn mulitply(x: i32, y: i32) -> i32 {
-//     x * y
-// }
-
-// fn compute<F>(function: F, a: i32, b: i32)
-fn compute<F>(function: F)
-where
-    // F: Fn(i32, i32) -> i32,
-    F: Fn() -> i32,
-{
-    println!("{}", function());
+    let a = String::from("Hello");
+    let clos = || {
+        println!("{}", a);
+    };
+    clos();
+    clos();
+    clos();
+    clos();
 }
