447e66f63cb2a9319838746cabb58523a27bc521
Puech Fabien (CSgroup)~ <fabien.puech@csgroup.eu>
2023-01-23 11:08:32 +0100

    generic simple

--- base_rust/src/mon_module/generic.rs
+++ base_rust/src/mon_module/generic.rs
@@ -1,9 +1,19 @@
-struct Rectangle<T, U> {
+struct Rectangle<T> {
     x: T,
-    y: U,
+    y: T,
+}
+
+impl<T> Rectangle<T> {
+    fn valeur(&self) -> &T {
+        &self.x
+    }
+    fn surface(&self) -> &T {
+        &self.x * &self.y
+    }
 }
 
 pub fn hello() {
     let r1 = Rectangle { x: 10, y: 20 };
-    let r2 = Rectangle { x: 2.0, y: 5 };
+    let r2 = Rectangle { x: 2.7, y: 5.9 };
+    println!("r1.x = {}", r2.surface());
 }
