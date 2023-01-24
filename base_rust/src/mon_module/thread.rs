a290284712784a308b31df557907065c13e1c69e
Puech Fabien (CSgroup)~ <fabien.puech@csgroup.eu>
2023-01-24 09:54:35 +0100

    multiple sender

--- base_rust/src/mon_module/thread.rs
+++ base_rust/src/mon_module/thread.rs
@@ -4,11 +4,19 @@ use std::time::Duration;
 
 pub fn hello() {
     let (sender, receiver) = mpsc::channel();
+    let sender2 = mpsc::Sender::clone(&sender);
 
     thread::spawn(move || {
         for i in 1..10 {
             sender.send(i).unwrap();
             thread::sleep(Duration::from_millis(500));
+        }
+    });
+
+    thread::spawn(move || {
+        for i in 10..20 {
+            sender2.send(i).unwrap();
+            thread::sleep(Duration::from_millis(200));
         }
     });
 
