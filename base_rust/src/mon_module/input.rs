d5bbf064731a80f74f2e8c9f2d56c3319259c99d
Puech Fabien (CSgroup)~ <fabien.puech@csgroup.eu>
2023-01-24 11:06:26 +0100

    fs openoptions

--- base_rust/src/mon_module/input.rs
+++ base_rust/src/mon_module/input.rs
@@ -1,18 +1,15 @@
-use std::fs::File;
-use std::io::{stdin, BufRead, BufReader, Write};
+use std::fs::OpenOptions;
+use std::io::{stdin, Write};
+
 pub fn hello() {
-    let path = "test.txt";
+    let mut file = OpenOptions::new()
+        .write(true)
+        .append(true)
+        .create(true)
+        .open("test.txt")
+        .unwrap();
 
     let mut s = String::new();
     stdin().read_line(&mut s).unwrap();
-
-    let mut output = File::create(path).unwrap();
-    output.write_all(s.to_uppercase().as_bytes()).unwrap();
-
-    let input = File::open(path).unwrap();
-    let buffered = BufReader::new(input);
-
-    for line in buffered.lines() {
-        println!("{}", line.unwrap());
-    }
+    file.write(s.as_bytes()).unwrap();
 }
